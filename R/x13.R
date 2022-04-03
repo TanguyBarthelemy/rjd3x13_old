c9bc7bca75bead6e816d5dd20d815c27a0a2bbfe
AQlT <alain.quartier@yahoo.fr>
2022-04-03 12:47:10 +0200

    documentation

--- R/x13.R
+++ R/x13.R
@@ -4,25 +4,28 @@ NULL
 #' RegARIMA model, pre-adjustment in X13
 #'
 #' @param ts a univariate time series
-#' @param spec the model specification. Can be either the name of a predifined specification (`"rg0"`, `"rg1"`, `"rg2c"`, `"rg3"`, `"rg4"` or `"rg5c"`, see details) or a user-defined specification.
+#' @param spec the model specification. Can be either the name of a predifined specification or a user-defined specification.
 #' @param context the dictionnary of variables.
-#' @details
-#' The available predefined 'JDemetra+' model specifications are described in the table below:
 #'
-#' \tabular{rrrrrrr}{
-#' \strong{Identifier} |\tab \strong{Log/level detection} |\tab \strong{Outliers detection} |\tab \strong{Calendar effects} |\tab \strong{ARIMA}\cr
-#' RG0 |\tab \emph{NA} |\tab \emph{NA} |\tab \emph{NA} |\tab Airline(+mean)\cr
-#' RG1 |\tab automatic |\tab AO/LS/TC  |\tab \emph{NA} |\tab Airline(+mean)\cr
-#' RG2c |\tab automatic |\tab AO/LS/TC |\tab 2 td vars + Easter |\tab Airline(+mean)\cr
-#' RG3 |\tab automatic |\tab AO/LS/TC |\tab \emph{NA} |\tab automatic\cr
-#' RG4c |\tab automatic |\tab AO/LS/TC |\tab 2 td vars + Easter |\tab automatic\cr
-#' RG5c |\tab automatic |\tab AO/LS/TC |\tab 7 td vars + Easter |\tab automatic
-#' }
-#' @md
-#' @return
-#' @export
+#' @return the `regarima()` function returns a list with the results (`"JD3_REGARIMA_RSLTS"` object), the estimation specification and the result specification, while `fast.regarima()` is a faster function that only returns the results.
 #'
 #' @examples
+#' y = rjd3toolkit::ABS$X0.2.09.10.M
+#' sp = spec_regarima_default("rg5c")
+#' sp = add_outlier(sp,
+#'                  type = c("AO"), c("2015-01-01", "2010-01-01"))
+#' fast.regarima(y, spec = sp)
+#' sp = set_transform(
+#'   set_tradingdays(
+#'     set_easter(sp, enabled = FALSE),
+#'     option = "workingdays"
+#'   ),
+#'   fun = "None"
+#' )
+#' fast.regarima(y, spec = sp)
+#' sp = set_outlier(sp, outliers.type = c("AO"))
+#' fast.regarima(y, spec = sp)
+#' @export
 regarima<-function(ts, spec="rg4", context=NULL){
   jts<-rjd3toolkit::ts_r2jd(ts)
   if (is.character(spec)){
@@ -40,7 +43,6 @@ regarima<-function(ts, spec="rg4", context=NULL){
     return (regarima_output(jrslt))
   }
 }
-
 #' @export
 #' @rdname regarima
 fast.regarima<-function(ts, spec="rg4", context=NULL){
@@ -52,7 +54,7 @@ fast.regarima<-function(ts, spec="rg4", context=NULL){
     if (is.null(context)){
       context<-.jnull("demetra/util/r/Dictionary")
     }
-    jrslt<-.jcall("demetra/x13/r/RegArima", "Ljdplus/regsarima/regular/RegSarimaModel;", "process", jts, jspec, context )
+    jrslt<-.jcall("demetra/x13/r/RegArima", "Ljdplus/regsarima/regular/RegSarimaModel;", "process", jts, jspec, context)
   }
   if (is.jnull(jrslt)){
     return (NULL)
@@ -78,22 +80,27 @@ regarima_output<-function(jq){
 #' Seasonal Adjustment with  X13-ARIMA
 #'
 #' @inheritParams regarima
-#' @param spec the model specification. Can be either the name of a predifined specification (`"rsa0"`, `"rsa1"`, `"rsa2c"`, `"rsa3"`, `"rsa4"` or `"rsa5c"`, see details) or a user-defined specification.
-#'
-#' The available predefined 'JDemetra+' model specifications are described in the table below:
-#'
-#' \tabular{rrrrrrr}{
-#' \strong{Identifier} |\tab \strong{Log/level detection} |\tab \strong{Outliers detection} |\tab \strong{Calendar effects} |\tab \strong{ARIMA}\cr
-#' RSA0 |\tab \emph{NA} |\tab \emph{NA} |\tab \emph{NA} |\tab Airline(+mean)\cr
-#' RSA1 |\tab automatic |\tab AO/LS/TC  |\tab \emph{NA} |\tab Airline(+mean)\cr
-#' RSA2c |\tab automatic |\tab AO/LS/TC |\tab 2 td vars + Easter |\tab Airline(+mean)\cr
-#' RSA3 |\tab automatic |\tab AO/LS/TC |\tab \emph{NA} |\tab automatic\cr
-#' RSA4c |\tab automatic |\tab AO/LS/TC |\tab 2 td vars + Easter |\tab automatic\cr
-#' RSA5c |\tab automatic |\tab AO/LS/TC |\tab 7 td vars + Easter |\tab automatic\cr
-#' }
-#' @export
+#' @param spec the model specification. Can be either the name of a predifined specification or a user-defined specification.
 #'
 #' @examples
+#' sp = spec_x13_default("rg5c")
+#' y = rjd3toolkit::ABS$X0.2.09.10.M
+#' fast.x13(y, spec = sp)
+#' sp = add_outlier(sp,
+#'                  type = c("AO"), c("2015-01-01", "2010-01-01"))
+#' sp = set_transform(
+#'   set_tradingdays(
+#'     set_easter(sp, enabled = FALSE),
+#'     option = "workingdays"
+#'   ),
+#'   fun = "None"
+#' )
+#' sp = set_x11(sp,
+#'              henderson.filter = 13)
+#' fast.x13(y, spec = sp)
+#'
+#' @return the `regarima()` function returns a list with the results (`"JD3_REGARIMA_RSLTS"` object), the estimation specification and the result specification, while `fast.regarima()` is a faster function that only returns the results.
+#' @export
 x13<-function(ts, spec="rsa4", context=NULL){
   jts<-rjd3toolkit::ts_r2jd(ts)
   if (is.character(spec)){
@@ -148,16 +155,19 @@ x13_output<-function(jq){
 
 }
 
-#' X11 Decomposition Algorithm
+#' X-11 Decomposition Algorithm
 #'
 #' @inheritParams x13
-#' @param spec the specification
-#'
-#' @return
-#' @export
+#' @param spec the specification.
 #'
 #' @examples
-x11<-function(ts, spec){
+#' y <- rjd3toolkit::ABS$X0.2.09.10.M
+#' x11_spec <- spec_x11_default()
+#' x11(y, x11_spec)
+#' x11_spec <- set_x11(x11_spec, henderson.filter = 13)
+#' x11(y, x11_spec)
+#' @export
+x11 <- function(ts, spec){
   jts<-rjd3toolkit::ts_r2jd(ts)
   jspec<-r2jd_spec_x11(spec)
   jrslt<-.jcall("demetra/x13/r/X11", "Ljdplus/x11/X11Results;", "process", jts, jspec)
@@ -168,19 +178,33 @@ x11<-function(ts, spec){
   }
 }
 
-#' Title
+#' Refresh Policy
 #'
-#' @param spec
-#' @param refspec
-#' @param policy
-#' @param period
-#' @param start
-#' @param end
+#' @param spec the current specification
+#' @param refspec the reference specification. By default this is the `"RG4"` or `"RSA4"` specification. TODO to explain.
+#' @param policy the refresh policy
+#' @param period,start,end to specify the frozen domain when `policy` equals to `"Outliers"` or `"Outliers_StochasticComponent"`.
 #'
-#' @return
-#' @export
-#'
+#' @return a new specification
 #' @examples
+#' y = rjd3toolkit::ABS$X0.2.08.10.M
+#' y_anc = window(y,end = 2009)
+#' mod_anc = regarima(y_anc, spec_regarima_default())
+#' res_spec = mod_anc$result_spec
+#' mod_anc
+#' # ARIMA parameters fixed
+#' fast.regarima(y,
+#'               regarima.refresh(res_spec,
+#'                                mod_anc$estimation_spec,
+#'                                policy = "FixedParameters"))
+#' # Outlier detection
+#' fast.regarima(y,
+#'               regarima.refresh(res_spec,
+#'                                policy = "Outliers"))
+#'
+#' @name refresh
+#' @rdname refresh
+#' @export
 regarima.refresh<-function(spec, refspec=NULL, policy=c("FreeParameters", "Complete", "Outliers_StochasticComponent", "Outliers", "FixedParameters", "FixedAutoRegressiveParameters", "Fixed"), period=0, start=NULL, end=NULL){
   policy=match.arg(policy)
   if (class(spec) != "JD3_REGARIMA_SPEC") stop("Invalid specification type")
@@ -197,19 +221,8 @@ regarima.refresh<-function(spec, refspec=NULL, policy=c("FreeParameters", "Compl
   return (jd2r_spec_regarima(jnspec))
 }
 
-#' Title
-#'
-#' @param spec
-#' @param refspec
-#' @param policy
-#' @param period
-#' @param start
-#' @param end
-#'
-#' @return
+#' @rdname refresh
 #' @export
-#'
-#' @examples
 x13.refresh<-function(spec, refspec=NULL, policy=c("FreeParameters", "Complete", "Outliers_StochasticComponent", "Outliers", "FixedParameters", "FixedAutoRegressiveParameters", "Fixed", "Current"), period=0, start=NULL, end=NULL){
   policy=match.arg(policy)
   if (class(spec) != "JD3_X13_SPEC") stop("Invalid specification type")
@@ -224,6 +237,5 @@ x13.refresh<-function(spec, refspec=NULL, policy=c("FreeParameters", "Complete",
   jdom<-rjd3toolkit::jdomain(period, start, end)
   jnspec<-.jcall("demetra/x13/r/X13", "Ldemetra/x13/X13Spec;", "refreshSpec", jspec, jrefspec, jdom, policy)
   return (jd2r_spec_x13(jnspec))
-
 }
 
