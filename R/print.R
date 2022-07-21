a74ecc33c1fa8946f9caa1521b47d34f547a7992
AQlT <alain.quartier@yahoo.fr>
2022-07-21 16:58:57 +0200

    print functions

---
+++ R/print.R
@@ -0,0 +1,78 @@
+#'@importFrom stats printCoefmat
+#'@importFrom utils capture.output
+print_x11_decomp <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
+  mstats = matrix(unlist(x$mstats),
+                  ncol = 1,
+                  dimnames = list(names(x$mstats), "M stats"))
+  cat("Monitoring and Quality Assessment Statistics:",
+      "\n")
+  printCoefmat(mstats, digits = digits, P.values= FALSE, na.print = "NA", ...)
+  cat("\n")
+  cat("Final filters:","\n")
+  cat("Seasonal filter: ",x$decomposition$final_seasonal)
+  cat("\n")
+  cat(sprintf("Trend filter: %s terms Henderson moving average", x$decomposition$final_henderson))
+  cat("\n")
+  invisible(x)
+}
+print_diagnostics <- function(x, digits = max(3L, getOption("digits") - 3L),
+                              ...){
+  variance_decomposition = x$diagnostics$vardecomposition
+  variance_decomposition = matrix(unlist(variance_decomposition),
+                                  ncol = 1,
+                                  dimnames = list(names(variance_decomposition), "Component"))
+  residuals_test = x$diagnostics[grep("test", names(x$diagnostics))]
+  residuals_test = lapply(residuals_test,function(test) test[["pvalue"]])
+  residuals_test = matrix(unlist(residuals_test),
+                          ncol = 1,
+                          dimnames = list(names(residuals_test), "P.value"))
+
+  cat("Relative contribution of the components to the stationary",
+      "portion of the variance in the original series,",
+      "after the removal of the long term trend (in %)",
+      sep = "\n"
+  )
+  cat("\n")
+  cat(paste0(" ",
+             capture.output(
+               printCoefmat(variance_decomposition*100, digits = digits, ...)
+             )),
+      sep ="\n")
+  cat("\n")
+
+  cat("Residual seasonality tests")
+  cat("\n")
+  cat(paste0(" ",
+             capture.output(
+               printCoefmat(residuals_test, digits = digits,
+                            na.print = "NA", ...)
+             )
+  ),
+  sep ="\n")
+  cat("\n")
+
+  invisible(x)
+}
+print_final <- function(x, ...){
+  print(rjd3sa::sa.decomposition(x), ...)
+  invisible(x)
+}
+
+#' @export
+print.JD3_X13_RSLTS <- function(x, digits = max(3L, getOption("digits") - 3L),
+                   ...){
+
+  cat("RegARIMA","\n",sep="")
+  print(x$preprocessing, digits = digits, ...)
+  cat("\n", "Decomposition","\n",sep="")
+  print_x11_decomp(x, digits = digits, ...)
+  cat("\n", "Diagnostics","\n",sep="")
+  print_diagnostics(x, digits = digits, ...)
+  cat("\n", "Final","\n",sep="")
+  print_final(x, digits = digits, ...)
+}
+#' @export
+print.JD3_X13_OUTPUT<- function(x, digits = max(3L, getOption("digits") - 3L),
+                                ...){
+  print(x$result, digits = digits, ...)
+}
